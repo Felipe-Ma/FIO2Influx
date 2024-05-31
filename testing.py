e4b1cd804795c4add13eeb12ef05dd0b5efa31e1
Felipe-Ma <70176706+Felipe-Ma@users.noreply.github.com>
2024-05-31 11:18:23 -0700

    code works but numbers off

--- testing.py
+++ testing.py
@@ -119,3 +119,4 @@ if __name__ == "__main__":
     org = input("Enter the organization: ")
     fio_job_file = input("Enter the FIO job file path: ")
     run_fio(fio_job_file, db_name, org, token)
+    # Code works at this point but numbers are off

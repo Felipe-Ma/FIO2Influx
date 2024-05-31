8a4244b99e80416f16173d5f55c6c559938b5852
Felipe-Ma710 <f_martinez8@u.pacific.edu>
2024-05-31 09:48:15 -0700

    json format

--- fioinflux2.py
+++ fioinflux2.py
@@ -1,6 +1,8 @@
 import subprocess
 import json
 from influxdb_client import InfluxDBClient, Point, WritePrecision
+from datetime import datetime
+
 
 # Function to run FIO and get the result as JSON
 def run_fio(fio_job_file):
@@ -12,25 +14,39 @@ def run_fio(fio_job_file):
         print(f"Error running FIO: {e}")
         return None
 
+
 # Function to write FIO result to InfluxDB
 def write_to_influxdb(db_name, token, org, fio_result):
     client = InfluxDBClient(url="http://localhost:8086", token=token)
-    #write_api = client.write_api(write_options=WritePrecision.NS)
-    write_api = client.write_api(write_options=WriteOptions(batch_size=1))
-
+    write_api = client.write_api(write_options=WritePrecision.NS)
 
     for job in fio_result['jobs']:
-        point = Point("fio") \
-            .tag("jobname", job['jobname']) \
-            .field("read_bw", job['read']['bw']) \
-            .field("read_iops", job['read']['iops']) \
-            .field("write_bw", job['write']['bw']) \
-            .field("write_iops", job['write']['iops'])
+        hostname = job.get('hostname', 'unknown')
+        jobname = job['jobname']
+        current_time = datetime.utcnow().isoformat()
 
-        write_api.write(bucket=db_name, org=org, record=point)
+        read = job['read']
+
+        json_body = [
+            {
+                "measurement": "FIO",
+                "tags": {
+                    "runId": jobname,
+                    "hostname": hostname
+                },
+                "time": current_time,
+                "fields": {
+                    "Read_IOPS": read['iops'],
+                    "Read_bandwidth_(MB/s)": read['bw']
+                }
+            }
+        ]
+
+        write_api.write(bucket=db_name, org=org, record=json_body)
 
     client.close()
     print("FIO result written to InfluxDB.")
+
 
 # Main script
 if __name__ == "__main__":
